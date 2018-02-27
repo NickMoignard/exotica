92752613a8ba102dea7ce3c16d73105cac29263c
nickmoignard <nicklmoignard@gmail.com>
2018-02-27 16:44:33 +1100

    added JSON responses and RESTful api for the set times controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :stages
   resources :dancers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :set_times
+      resources :stages
+      resources :dancers
+    end
+  end
 end
